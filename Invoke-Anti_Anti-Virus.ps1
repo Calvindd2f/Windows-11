28075e2f144ddf4ad41892cfd8aff08b78e59b74
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-29 11:21:08 +0100

    Update Invoke-Anti_Anti-Virus.ps1

--- Invoke-Anti_Anti-Virus.ps1
+++ Invoke-Anti_Anti-Virus.ps1
@@ -3,3 +3,126 @@ $computername=$env:computername
 $AntiVirusProducts = Get-WmiObject -Namespace "root\SecurityCenter2" -Class AntiVirusProduct  -ComputerName $computername
 
 # To Do
+
+#Invoke-Anti_AntiVirus
+#https://github.com/Calvindd2f/Windows-10-11/blob/main/Invoke-Anti_Anti-Virus.ps1
+
+#$computername=$env:computername
+#$AntiVirusProducts = Get-WmiObject -Namespace "root\SecurityCenter2" -Class AntiVirusProduct  -ComputerName $env:computername
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<#
+$computerList = "localhost", "localhost"
+$filter = "antivirus"
+
+$results = @()
+foreach($computerName in $computerList) {
+
+    $hive = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $computerName)
+    $regPathList = "SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
+                   "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+
+    foreach($regPath in $regPathList) {
+        if($key = $hive.OpenSubKey($regPath)) {
+            if($subkeyNames = $key.GetSubKeyNames()) {
+                foreach($subkeyName in $subkeyNames) {
+                    $productKey = $key.OpenSubKey($subkeyName)
+                    $productName = $productKey.GetValue("DisplayName")
+                    $productVersion = $productKey.GetValue("DisplayVersion")
+                    $productComments = $productKey.GetValue("Comments")
+                    if(($productName -match $filter) -or ($productComments -match $filter)) {
+                        $resultObj = [PSCustomObject]@{
+                            Host = $computerName
+                            Product = $productName
+                            Version = $productVersion
+                            Comments = $productComments
+                        }
+                        $results += $resultObj
+                    }
+                }
+            }
+        }
+        $key.Close()
+    }
+}
+
+$results | ft -au
+#>
